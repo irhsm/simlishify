53484b995be379bdd64aa0c89c8b445b3a6dc2e5
Irhas <irhasm@outlook.com>
2025-10-11 13:28:32 +0700

    feat: add graphic and modal

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
-  import '../../app.css'
+  import { app } from '$lib/shared.svelte'
+  import { simlish } from '$lib/simlish'
+  import { wordBank } from '$lib/wordBank'
+
   let { children } = $props()
 </script>
 
 <div class="drawer-content">
+  <dialog class="modal" class:modal-open={app.isModalOpen}>
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">Hello!</h3>
+      <p class="py-4">Press ESC key or click the button below to close</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn" onclick={() => (app.isModalOpen = false)}>Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
   {@render children?.()}
 </div>
 <div class="drawer-side">
   <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
   <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-    <!-- Sidebar content here -->
-    <li><button>Sidebar Item 1</button></li>
-    <li><button>Sidebar Item 2</button></li>
+    <li><button>Memoized pairs: {Object.keys(app.memoized).length}</button></li>
+    <li><button>Known Simlish word: {Object.keys(simlish).length}</button></li>
+    <li><button>Word bank: {wordBank.length}</button></li>
   </ul>
 </div>
